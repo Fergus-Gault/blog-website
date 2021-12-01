56c68a081d8b498cc5ca19f2e21b3547c6fdb850
Fergus-Gault <fergus@gault.com>
2021-12-01 21:55:01 +0000

    Added email field for register

--- website/auth.py
+++ website/auth.py
@@ -15,28 +15,35 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 def register():
     if request.method == 'POST':
         username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
+        elif not email:
+            error = 'Email is required.'
         elif not password:
             error = 'Password is required.'
         
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (id, username, password) VALUES (?,?,?)",
-                    (shortuuid.uuid(),username, generate_password_hash(password)),
+                    "INSERT INTO user (id, username, email, password) VALUES (?,?,?,?)",
+                    (shortuuid.uuid(), username, email, generate_password_hash(password)),
                 )
                 db.commit()
                 user = db.execute(
                     'SELECT * FROM user WHERE username = ?', (username,)
                     ).fetchone()
                 session['user_id'] = user['id']
-            except db.IntegrityError:
-                error = f'User {username} is already registered.'
+
+            except db.IntegrityError as e:
+                if "username" in str(e):
+                    error = f'Username is already registered.'
+                elif "email" in str(e):
+                    error = f'Email is already registered.'
             else:
                 return redirect(url_for('blog.index'))
         
