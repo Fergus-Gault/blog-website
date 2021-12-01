5c57d6e0f1498222b44705032048a07b8926eab7
Fergus-Gault <fergus@gault.com>
2021-12-01 21:55:54 +0000

    Added option to add and remove admin permissions

--- website/db.py
+++ website/db.py
@@ -3,6 +3,8 @@ from sqlite3.dbapi2 import register_adapter
 
 import shortuuid
 
+from werkzeug.security import generate_password_hash
+
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
@@ -30,6 +32,22 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+    db.execute(
+        'INSERT INTO user (id, username, email, password, admin, emailConfirmed)'
+        ' VALUES (?,?,?,?,?,?)', (shortuuid.uuid(), 'admin', 'ad@min.com', generate_password_hash('root'), 1, 1)
+        )
+    db.commit()
+
+def make_admin(username):
+    db = get_db()
+    db.execute('UPDATE user SET admin=1, emailConfirmed=1 WHERE username = ?', (username,))
+    db.commit()
+
+def remove_admin(username):
+    db = get_db()
+    db.execute('UPDATE user SET admin=0, emailConfirmed=1 WHERE username = ?', (username,))
+    db.commit()
+
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
@@ -38,6 +56,26 @@ def init_db_command():
     click.echo('Initialised the database.')
 
 
+@click.command('make-admin')
+@click.argument('username')
+@with_appcontext
+def make_admin_command(username):
+    # Makes user an admin.
+    make_admin(username)
+    click.echo(f'{username} is now an admin.')
+
+
+@click.command('remove-admin')
+@click.argument('username')
+@with_appcontext
+def remove_admin_command(username):
+    # Removes admin from user
+    remove_admin(username)
+    click.echo(f'{username} is no longer an admin.')
+
+
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+    app.cli.add_command(make_admin_command)
+    app.cli.add_command(remove_admin_command)
